8ca7b98efcd60262a37622c016026fa4839c641c
BobC <bob@crowley.software>
2020-05-09 08:19:50 -0400

    Added middleware to provide message and link from root of app.

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -31,6 +32,11 @@ namespace IGAS
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.MapWhen(c => c.Request.Path.Value == "/", 
+                app => app.Run(async ctx => { 
+                    await ctx.Response.WriteAsync($"<p>The API is running. Go to <a href=\"{ctx.Request.PathBase.Add("/Configuration").Value}\">/Configuration</a> to see your data.</p>");
+                }));
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -39,8 +45,6 @@ namespace IGAS
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
